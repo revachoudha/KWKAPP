0c8eb89063e6650ed453fb0ec807f1902d216f08
Reva Choudha <revachoudha@gmail.com>
2024-07-26 10:52:57 -0500

    Initial Commit

---
+++ KWKAPP/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  KWKAPP
+//
+//  Created by Reva Choudha on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var textTitle = "What is your name?"
+    @State private var name = ""
+    
+    var body: some View {
+        VStack {
+            Text(textTitle)
+            
+            TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+                .padding()
+            
+            Button("Submit Name") {
+                textTitle = "Welcome, \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            .padding()
+        }
+        .padding(50)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KWKAPP/KWKAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWKAPPApp.swift
+//  KWKAPP
+//
+//  Created by Reva Choudha on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct KWKAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
